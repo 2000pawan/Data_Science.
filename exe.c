8c2ce93d657341343e423bb124b4b7745a043ac7
2000pawan <yaduvanshi2000pawan@gmail.com>
2024-10-04 13:19:38 +0530

    Commit

---
+++ exe.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+printf("Enter Two numbers:-\n");
+scanf("%d%d",&a,&b);
+printf("Addition of Two numbers %d + %d is :- %d\n",a,b,a+b);
+printf("Hii, Pawan Yadav");
+}
